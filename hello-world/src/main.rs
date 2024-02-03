ca1bc9f2722ad4f6820acc1b3d72d1bb95ad9b3c
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-02-03 23:01:09 +0700

    Section 2: Variables

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    // VARIABLES
+    let var_a = "Variable A value";
+
+    let mut var_b = 1;
+    var_b += 1;
+
     println!("Hello Rust bootcamp 2024!");
-    print!("Hello world 123!")
+    println!("var_a {var_a}");
+    println!("var_b {}", var_b);
 }
