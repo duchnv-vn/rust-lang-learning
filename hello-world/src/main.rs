81d3e6e80cc8872be4918ceafd7517df78f70af4
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-02-03 23:47:34 +0700

    Section 2: Type casting

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,15 +1,25 @@
 fn main() {
-    // VARIABLES
-    let var_a = "Variable A value";
+    println!("Hello Rust bootcamp 2024!");
 
-    let mut var_b = 1;
-    var_b += 1;
+    // VARIABLES
+    let _var_a = "Variable A value";
+
+    let mut _var_b = 1;
+    _var_b += 1;
 
     const _PI: f32 = 3.14;
-    let arr = [1, 2, 3, 4, 5];
+    let _arr = [1, 2, 3, 4, 5];
 
-    println!("Hello Rust bootcamp 2024!");
-    println!("var_a {var_a}");
-    println!("var_b {}", var_b);
-    println!("arr {}", arr[4]);
+    // DATA CASTING
+    let x: u16 = 300;
+    let _y = x as u8;
+
+    let char_1 = 'A';
+    let _num_from_char_1 = char_1 as i8;
+
+    let bool_1 = false;
+    let _num_from_bool_1 = bool_1 as i8;
+
+    let int_1: u8 = 54;
+    let _char_from_int_1 = int_1 as char;
 }
