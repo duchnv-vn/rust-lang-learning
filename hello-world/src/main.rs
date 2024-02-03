cc26f4b59618d21d6d0f71b80d778d798d405b71
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-02-03 23:34:59 +0700

    Section 2: Data types

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -5,7 +5,11 @@ fn main() {
     let mut var_b = 1;
     var_b += 1;
 
+    const _PI: f32 = 3.14;
+    let arr = [1, 2, 3, 4, 5];
+
     println!("Hello Rust bootcamp 2024!");
     println!("var_a {var_a}");
     println!("var_b {}", var_b);
+    println!("arr {}", arr[4]);
 }
