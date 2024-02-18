4595df4bde1b94ac91e53d4859f2cfbd20144564
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-02-18 21:54:52 +0700

    Section 4: Rust functions

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -80,6 +80,60 @@ fn main() {
 
     // ---------------------------------------------------------
     // SECTION 4: FUNCTIONS
+    /* fn hello_func(first_name: &str, last_name: &str) {
+        println!("Hello {first_name} {last_name}!")
+    }
+
+    hello_func("Duc", "Huynh"); */
+
+    /* fn add_func(array: &[i8]) -> i8 {
+        let mut sum: i8 = 0;
+
+        for number in array {
+            sum += number
+        }
+
+        sum
+    }
+
+    let sum_result = add_func(&[1, 2, 3]);
+    println!("Sum result is {sum_result}"); */
+
+    /*
+       EXERCISE 1
+    */
+    let array_need_find_max: &[i32] = &[9, 66, 96, 1, -11, 0];
+    fn find_max(array: &[i32]) -> &i32 {
+        let mut max: &i32 = &0;
+
+        for number in array {
+            if number > max {
+                max = number;
+            }
+        }
+
+        max
+    }
+    let max_number = find_max(array_need_find_max);
+    println!("Max number is {max_number}");
+
+    /*
+       EXERCISE 2
+    */
+    let array_need_to_find_even: &[i32] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    fn filter_even_array(array: &[i32]) -> Vec<&i32> {
+        let mut result_array: Vec<&i32> = vec![];
+
+        for number in array {
+            if number % 2 == 0 {
+                result_array.push(number)
+            }
+        }
+
+        result_array
+    }
+    let filltered_array = filter_even_array(array_need_to_find_even);
+    println!("Filtered array is {:?}", filltered_array);
 
     println!("----- End of main func! -----");
 }
