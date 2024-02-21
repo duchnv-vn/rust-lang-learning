23ccf06c18d3e86679e6016541096c14e06851cf
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-02-21 23:46:34 +0700

    section 5: stack/heap and ownership/borrowing

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -147,26 +147,65 @@ fn main() {
     /*
        EXERCISE 1
     */
-    let array_need_find_max_closure = [9, 66, 96, 1, -11, 0];
+    /* let array_need_find_max_closure = [9, 66, 96, 1, -11, 0];
     let mut max_number_closure: &i32 = &0;
     array_need_find_max_closure.iter().for_each(|number| {
         if number > max_number_closure {
             max_number_closure = number;
         }
     });
-    println!("Max number is {max_number_closure}");
+    println!("Max number is {max_number_closure}"); */
 
     /*
        EXERCISE 2
     */
-    let array_need_to_find_odd: &[i32] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    /* let array_need_to_find_odd: &[i32] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let mut filltered_array_closure: Vec<&i32> = vec![];
     array_need_to_find_odd.iter().for_each(|number| {
         if number % 2 != 0 {
             filltered_array_closure.push(number)
         }
     });
-    println!("Filtered array is {:?}", filltered_array_closure);
+    println!("Filtered array is {:?}", filltered_array_closure); */
+
+    // ---------------------------------------------------------
+    // SECTION 5: STACK/HEAP, OWNERSHIP/BORROWING
+    // 5.1 OWNERSHIP
+    /* let var_1 = String::from("banana");
+    let var_2 = var_1;
+
+    println!("var = {}", var_2); */ // ownership of `banana` value transfer from var_1 to var_2 -> raise exception if want to print var_1
+
+    // 5.2 BORROWING
+    // 5.2.1 SHARED REFERENCE -> &
+    /* let var_6 = String::from("pinapple");
+    let var_5 = &var_6;
+    let var_4 = &var_5;
+    print_fruit(&var_6);
+    println!("var_6 = {}", var_6); */
+
+    // 5.2.2 WRITE REFERENCE -> &mut | borow to write with the acceptance from the owner
+    /* let mut var_7 = String::from("apple");
+    write_and_print_fruit(&mut var_7);
+    println!("var_7 = {}", var_7); */
+
+    /* let mut var_8 = String::from("cherry");
+    let var_10 = &mut var_8;
+    var_10.push_str(" lady");
+    let var_9 = &var_10;
+
+    println!("vars = {} {}", var_9, var_10); // cannot print var_8 in this line because it is being borrowed by other vars
+    println!("vars = {}", var_8); */
 
     println!("----- End of main func! -----");
+}
+
+fn write_and_print_fruit(str: &mut String) {
+    println!("str 1 = {}", str);
+    str.push_str(" red");
+    println!("str 2 = {}", str);
+}
+
+fn print_fruit(str: &String) {
+    println!("str = {}", str);
 }
