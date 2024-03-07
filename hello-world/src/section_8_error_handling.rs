37ee421489638bb6f7595f58a20d7fb4f87051f7
duchnv-icd <110797321+duchnv-icd@users.noreply.github.com>
2024-03-07 13:22:46 +0700

    Section 8: Custom error

--- hello-world/src/section_8_error_handling.rs
+++ hello-world/src/section_8_error_handling.rs
@@ -1,20 +1,20 @@
 use std::{fs::File, io::Read};
 
+// Result enum
+// enum Result<T, E> {
+//     Ok(T),  // Success process return value
+//     Err(E), // Failed process return error
+// }
+
+// Option enum
+// enum Option<T> {
+//     None,    // No value | same null
+//     Some(T), // Contain value
+// }
+
 pub fn main() {
     // Unrecoverable errors -> Stop program right away
     // panic!("exit");
-
-    // Result enum
-    // enum Result<T, E> {
-    //     Ok(T),  // Success process return value
-    //     Err(E), // Failed process return error
-    // }
-
-    // Option enum
-    // enum Option<T> {
-    //     None,    // No value | same null
-    //     Some(T), // Contain value
-    // }
 
     // Recoverable errors -> No stop program
     // let file_name = "data.txt";
@@ -31,17 +31,34 @@ pub fn main() {
     // println!("data_file: {:?}", data_file.read_to_string(&mut buffer));
 
     // Unwrap and expect
-    fn get_user_name(username: &str) -> Option<&str> {
-        if username.is_empty() {
-            return None;
-        }
+    // fn get_user_name(username: &str) -> Option<&str> {
+    //     if username.is_empty() {
+    //         return None;
+    //     }
 
-        Some(username)
-    }
+    //     Some(username)
+    // }
 
-    let unwrap_case = get_user_name("user 1").unwrap(); // stop program right away if error
-    let expect_case = get_user_name("user 1").expect("Not be empty"); // stop program right away if error
+    // let unwrap_case = get_user_name("user 1").unwrap(); // stop program right away if error
+    // let expect_case = get_user_name("user 2").expect("Not be empty"); // stop program right away if error
 
-    println!("unwrap_case: {}", unwrap_case);
-    println!("expect_case: {}", expect_case);
+    // println!("unwrap_case: {}", unwrap_case);
+    // println!("expect_case: {}", expect_case);
+
+    // #[derive(Debug)]
+    // enum CustomFileError {
+    //     FileOpenError,
+    //     FileReadError,
+    // }
+    // fn read_file_contents(path: &str) -> Result<String, CustomFileError> {
+    //     let mut file = File::open(path).map_err(|_| CustomFileError::FileOpenError)?;
+    //     let mut content = String::new();
+
+    //     file.read_to_string(&mut content)
+    //         .map_err(|_| CustomFileError::FileReadError)?;
+
+    //     Ok(content)
+    // }
+    // let file_content = read_file_contents("data.csv");
+    // println!("file_content: {:?}", file_content.unwrap());
 }
