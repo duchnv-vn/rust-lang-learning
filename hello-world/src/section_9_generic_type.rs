3ae716ba7be91763cb43f8954078434743257be1
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-03-17 18:37:59 +0700

    Section 9: Use generic type

--- hello-world/src/section_9_generic_type.rs
+++ hello-world/src/section_9_generic_type.rs
@@ -1,1 +1,26 @@
-pub fn main() {}
+pub fn main() {
+    // struct Point {
+    //     x: f64,
+    //     y: f64,
+    // }
+    // let point_1 = Point { x: 16f64, y: 16f64 };
+
+    // struct PointGen<T> {
+    //     x: T,
+    //     y: T,
+    // }
+    // let point_2: PointGen<f64> = PointGen { x: 16f64, y: 16f64 };
+
+    // struct PointGens<T, U> {
+    //     x: T,
+    //     y: U,
+    // }
+    // let point_3 = PointGens { x: 16f64, y: 16f32 };
+
+    // fn bar<T>(a: T) -> T {
+    //     a
+    // }
+
+    // let a_1 = bar(1.0);
+    // let a_2 = bar::<u16>(10);
+}
